1170ae08e61081016fb8f26a163084e8ee910de1
philbertshea <philbertshea@gmail.com>
2025-01-16 17:23:13 +0800

    Edit main in Phil.java to store tasks and print them using list.

--- src/main/java/Phil.java
+++ src/main/java/Phil.java
@@ -8,20 +8,28 @@ public class Phil {
     }
 
     public static void main(String[] args) {
+        String[] taskList = new String[100];
         Scanner sc = new Scanner(System.in);
-        printOutput("Hello. I'm Phil.\nWhat can I do for you?");
-        int inputCount = 0;
+        int taskCount = 0;
+        printOutput("Hello. I'm Phil.\nWhat can I do for you?" +
+                    "\nTip: say 'list' to get a list of actions to do, say 'bye' to end the conversation.");
+
         while (sc.hasNextLine()) {
-            inputCount++;
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 printOutput("Bye. Hope to see you again soon.");
                 break;
-            } else {
-                if (inputCount % 10 == 0) {
-                    input += "\nTo end our conversation, say bye.";
+            } else if (input.equals("list")) {
+                StringBuilder listToPrint = new StringBuilder();
+                for (int i = 0; i < taskCount; i++) {
+                    listToPrint.append((i + 1)).append(". ").append(taskList[i]).append("\n");
                 }
-                printOutput(input);
+                printOutput(listToPrint.toString());
+
+            } else {
+                taskList[taskCount] = input;
+                taskCount++;
+                printOutput("added: " + input);
             }
         }
     }
