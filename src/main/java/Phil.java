9c79239c7e94ebd5d657f7f815cf29f296940619
philbertshea <philbertshea@gmail.com>
2025-01-16 16:40:00 +0800

    Added printOutput function and edited main in Phil.java to repeat user input until user says bye.

--- src/main/java/Phil.java
+++ src/main/java/Phil.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Phil {
-    public static void main(String[] args) {
-        System.out.println("Hello. I'm Phil.\nWhat can I do for you?");
+    public static void printOutput(String output) {
         System.out.println("\n-----------------------------------------\n");
-        System.out.println("Bye. Hope to see you again soon.");
+        System.out.println(output);
+        System.out.println("\n-----------------------------------------\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        printOutput("Hello. I'm Phil.\nWhat can I do for you?");
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                printOutput("Bye. Hope to see you again soon.");
+                break;
+            } else {
+                printOutput(input);
+            }
+        }
     }
 }
