19b9d2ad827480d409c5b00255fd323e79a450c0
philbertshea <philbertshea@gmail.com>
2025-01-16 16:29:06 +0800

    Refactor Duke.java to Phil.java and print required skeleton message.

---
+++ src/main/java/Phil.java
@@ -0,0 +1,7 @@
+public class Phil {
+    public static void main(String[] args) {
+        System.out.println("Hello. I'm Phil.\nWhat can I do for you?");
+        System.out.println("\n-----------------------------------------\n");
+        System.out.println("Bye. Hope to see you again soon.");
+    }
+}
