cef4b03d085b19d3c09980a4a5a5d0c128f15acf
philbertshea <philbertshea@gmail.com>
2025-01-16 17:52:00 +0800

    Added Task class, and edited main in Phil.java to add marking functionality.

--- src/main/java/Phil.java
+++ src/main/java/Phil.java
@@ -8,7 +8,7 @@ public class Phil {
     }
 
     public static void main(String[] args) {
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         Scanner sc = new Scanner(System.in);
         int taskCount = 0;
         printOutput("Hello. I'm Phil.\nWhat can I do for you?" +
@@ -22,12 +22,25 @@ public class Phil {
             } else if (input.equals("list")) {
                 StringBuilder listToPrint = new StringBuilder();
                 for (int i = 0; i < taskCount; i++) {
-                    listToPrint.append((i + 1)).append(". ").append(taskList[i]).append("\n");
+                    listToPrint.append((i + 1)).append(". ").append(taskList[i].toString()).append("\n");
                 }
                 printOutput(listToPrint.toString());
 
+            } else if (input.startsWith("mark")) {
+                String[] inputArgs = input.split(" ");
+                if (inputArgs.length != 2 || !inputArgs[1].matches("\\d+") || Integer.parseInt(inputArgs[1]) > taskCount) {
+                    printOutput("Invalid input. To mark a task as done, say 'mark X' where X is the task to mark as done."
+                            + "\nMake sure X is a valid positive integer from 1 to " + taskCount + " (number of tasks). "
+                            + "\nFor example, calling 'mark 2' marks the second task as done."
+                            + "\nSay 'list' to see the tasks you have stored.");
+                } else {
+                    int taskToMark = Integer.parseInt(input.split(" ")[1]);
+                    taskList[taskToMark - 1].markDone();
+                    printOutput("Nice! I've marked this task as done: \n" + taskList[taskToMark - 1].toString());
+                }
+
             } else {
-                taskList[taskCount] = input;
+                taskList[taskCount] = new Task(input);
                 taskCount++;
                 printOutput("added: " + input);
             }
