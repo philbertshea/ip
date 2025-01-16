94ceb0744ec16734062ec4deaa6a995252c24875
philbertshea <philbertshea@gmail.com>
2025-01-16 16:47:01 +0800

    Edited main to remind user every 10 inputs that they can end the conversation.

--- src/main/java/Phil.java
+++ src/main/java/Phil.java
@@ -10,12 +10,17 @@ public class Phil {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         printOutput("Hello. I'm Phil.\nWhat can I do for you?");
+        int inputCount = 0;
         while (sc.hasNextLine()) {
+            inputCount++;
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 printOutput("Bye. Hope to see you again soon.");
                 break;
             } else {
+                if (inputCount % 10 == 0) {
+                    input += "\nTo end our conversation, say bye.";
+                }
                 printOutput(input);
             }
         }
