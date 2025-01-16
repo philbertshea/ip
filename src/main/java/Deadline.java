fe9a892191e11ff09b2b8b4f31933cff197f4fc3
philbertshea <philbertshea@gmail.com>
2025-01-16 18:06:24 +0800

    Made Task abstract and added Todo, Deadline, Event subclasses to Task.

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    public String byDate;
+
+    public Deadline(String description, String byDate) {
+        super(description);
+        this.byDate = byDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + this.byDate + ")";
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+    public String fromDate;
+    public String toDate;
+
+    public Event(String description, String fromDate, String toDate) {
+        super(description);
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(from: " + this.fromDate + " to: " + this.toDate + ")";
+    }
+}
