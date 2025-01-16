7af4ad7a7fbcc7adfe5f0eb88ae2eda34c7e5c8d
philbertshea <philbertshea@gmail.com>
2025-01-16 18:55:29 +0800

    Added TaskList class and edited Phil.java to use TaskList

--- src/main/java/Phil.java
+++ src/main/java/Phil.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Phil {
@@ -8,55 +10,66 @@ public class Phil {
     }
 
     public static void main(String[] args) {
-        Task[] taskList = new Task[100];
+        TaskList taskList = new TaskList();
         Scanner sc = new Scanner(System.in);
-        int taskCount = 0;
         printOutput("Hello. I'm Phil.\nWhat can I do for you?" +
                     "\nTip: say 'list' to get a list of actions to do, say 'bye' to end the conversation.");
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+            String[] inputArgs = input.split(" ");
             if (input.equals("bye")) {
                 printOutput("Bye. Hope to see you again soon.");
                 break;
             } else if (input.equals("list")) {
-                StringBuilder listToPrint = new StringBuilder();
-                listToPrint.append("Here are the tasks in your list: \n");
-                for (int i = 0; i < taskCount; i++) {
-                    listToPrint.append((i + 1)).append(". ").append(taskList[i].toString()).append("\n");
-                }
-                printOutput(listToPrint.toString());
+                printOutput(taskList.toString());
 
             } else if (input.startsWith("mark")) {
-                String[] inputArgs = input.split(" ");
-                if (inputArgs.length != 2 || !inputArgs[1].matches("\\d+") || Integer.parseInt(inputArgs[1]) > taskCount) {
+                int numTasks = taskList.getNumberOfTasks();
+                if (inputArgs.length != 2 || !inputArgs[1].matches("\\d+") || Integer.parseInt(inputArgs[1]) > numTasks) {
                     printOutput("Invalid input. To mark a task as done, say 'mark X' where X is the task to mark as done."
-                            + "\nMake sure X is a valid positive integer from 1 to " + taskCount + " (number of tasks). "
+                            + "\nMake sure X is a valid positive integer from 1 to " + numTasks + " (number of tasks). "
                             + "\nFor example, calling 'mark 2' marks the second task as done."
                             + "\nSay 'list' to see the tasks you have stored.");
                 } else {
                     int taskToMark = Integer.parseInt(input.split(" ")[1]);
-                    taskList[taskToMark - 1].markDone();
-                    printOutput("Nice! I've marked this task as done: \n" + taskList[taskToMark - 1].toString());
+                    printOutput(taskList.markTaskAsDone(taskToMark));
                 }
 
             } else if (input.startsWith("unmark")) {
-                String[] inputArgs = input.split(" ");
-                if (inputArgs.length != 2 || !inputArgs[1].matches("\\d+") || Integer.parseInt(inputArgs[1]) > taskCount) {
+                int numTasks = taskList.getNumberOfTasks();
+                if (inputArgs.length != 2 || !inputArgs[1].matches("\\d+") || Integer.parseInt(inputArgs[1]) > numTasks) {
                     printOutput("Invalid input. To mark a task as not done, say 'unmark X' where X is the task to mark as not done."
-                            + "\nMake sure X is a valid positive integer from 1 to " + taskCount + " (number of tasks). "
+                            + "\nMake sure X is a valid positive integer from 1 to " + numTasks + " (number of tasks). "
                             + "\nFor example, calling 'unmark 2' marks the second task as not done."
                             + "\nSay 'list' to see the tasks you have stored.");
                 } else {
                     int taskToMark = Integer.parseInt(input.split(" ")[1]);
-                    taskList[taskToMark - 1].markNotDone();
-                    printOutput("OK, I've marked this task as not done yet: \n" + taskList[taskToMark - 1].toString());
+                    printOutput(taskList.markTaskAsNotDone(taskToMark));
                 }
 
+            } else if (input.startsWith("todo")) {
+                if (inputArgs.length < 2) {
+                    printOutput("Invalid input. A Todo task requires a description of minimally one word."
+                                + "\n For example, 'todo read' creates the task 'read'.");
+                } else {
+                    // The whole input except for the first word 'todo' is the task description
+                    String description = String.join(" ", Arrays.copyOfRange(inputArgs, 1, inputArgs.length));
+                    printOutput(taskList.addTask(new Todo(description)));
+                }
+            } else if (input.startsWith("deadline")) {
+                List<String> inputArgsList = Arrays.asList(inputArgs);
+                if (inputArgs.length < 2 || !inputArgsList.contains("/by")) {
+                    printOutput("Invalid input. A Deadline task requires a description AND a deadline, specified as a string after '/by'."
+                            + "\n For example, 'deadline read /by Tuesday' creates the task 'read' with a deadline of 'Tuesday'.");
+                } else {
+                    int byIndex = inputArgsList.indexOf("/by");
+                    String description = String.join(" ", Arrays.copyOfRange(inputArgs, 1, byIndex));
+                    String byDate = String.join(" ", Arrays.copyOfRange(inputArgs, byIndex, inputArgs.length));
+                    printOutput(taskList.addTask(new Deadline(description, byDate)));
+                }
             } else {
-                taskList[taskCount] = new Task(input);
-                taskCount++;
-                printOutput("added: " + input);
+                printOutput("Invalid Input.");
             }
         }
     }
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    List<Task> listOfTasks;
+    public TaskList() {
+        this.listOfTasks = new ArrayList<Task>();
+    }
+
+    public int getNumberOfTasks() {
+        return listOfTasks.size();
+    }
+
+    public String addTask(Task task) {
+        listOfTasks.add(task);
+        return "Got it! I've added this task: \n" + task.toString()
+                + "\nNow you have " + this.getNumberOfTasks() + " tasks in the list.";
+    }
+
+    public String markTaskAsDone(int index) {
+        listOfTasks.get(index - 1).markDone();
+        return "Nice! I've marked this task as done: \n" + listOfTasks.get(index - 1).toString();
+    }
+
+    public String markTaskAsNotDone(int index) {
+        listOfTasks.get(index - 1).markNotDone();
+        return "OK, I've marked this task as not done: \n" + listOfTasks.get(index - 1).toString();
+    }
+
+    public String toString() {
+        StringBuilder listToPrint = new StringBuilder();
+        listToPrint.append("Here are the tasks in your list: \n");
+        for (int i = 0; i < listOfTasks.size(); i++) {
+            listToPrint.append((i + 1)).append(". ").append(listOfTasks.get(i).toString()).append("\n");
+        }
+        return listToPrint.toString();
+    }
+}
