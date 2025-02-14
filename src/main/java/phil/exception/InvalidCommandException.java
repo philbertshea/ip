3d9cacaeb1f3fcc2aa21f29949d5615820ea1313
philbertshea <philbertshea@gmail.com>
2025-02-14 17:47:39 +0800

    InvalidCommandException.java: Update message
    Message shown when exception is returned is too long,
    primarily due to the redundant new lines.
    
    Let's
    * Update the message to remove the new lines when
    an InvalidCommandException is thrown

--- src/main/java/phil/exception/InvalidCommandException.java
+++ src/main/java/phil/exception/InvalidCommandException.java
@@ -14,15 +14,15 @@ public class InvalidCommandException extends PhilException {
     public InvalidCommandException(String invalidCommand) {
         super(invalidCommand + " is not a valid command. \n"
                 + "These are the supported commands: \n"
-                + "'list' to see the list of tasks.\n\n"
-                + "'todo <some task>' to create a new Todo task. \n\n"
-                + "'deadline <some task> \\by <deadline-date>' to create a new Deadline task. \n\n"
-                + "'event <some task> \\from <start-date> \\end <end-date>' to create a new Event task. \n\n"
-                + "'delete <some task index>' to delete a task. \n\n"
-                + "'find <some search term>' to find a task by that search term. \n\n"
-                + "'new-note <some note>' to create a new note. \n\n"
-                + "'delete-note <some note index>' to delete a note. \n\n"
-                + "'list-note' to view a list of notes. \n\n"
-                + "'bye' to end the conversation. \n\n");
+                + "'list' to see the list of tasks.\n"
+                + "'todo <some task>' to create a new Todo task. \n"
+                + "'deadline <some task> \\by <deadline-date>' to create a new Deadline task. \n"
+                + "'event <some task> \\from <start-date> \\end <end-date>' to create a new Event task. \n"
+                + "'delete <some task index>' to delete a task. \n"
+                + "'find <some search term>' to find a task by that search term. \n"
+                + "'new-note <some note>' to create a new note. \n"
+                + "'delete-note <some note index>' to delete a note. \n"
+                + "'list-note' to view a list of notes. \n"
+                + "'bye' to end the conversation. \n");
     }
 }
